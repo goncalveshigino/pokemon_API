c0f5d4641e3da42722d2ca2b7c0d58d2e383816f
goncalveshigino <goncalveshigino20@gmail.com>
2023-06-02 14:13:32 +0100

    chore: gravando dados na db

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+
+import { Pokemon } from './entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+
+  constructor(
+
+    @InjectModel( Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+
+  ){}
+
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+
+    const pokemon = await this.pokemonModel.create( createPokemonDto )
+
+    return pokemon;
   }
 
   findAll() {
